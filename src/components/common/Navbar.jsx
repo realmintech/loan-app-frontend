7f41c16d83e71d2cc66472311fee77730bc2dd03
RealMinTech <Adesinamariam7@gmail.com>
2023-03-16 17:20:30 +0100

    setup

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+// import {Link} from "react-router-dom";
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>The navbar</h1>
+      <p>This is great.</p>
+    </div>
+  )
+};
